e51e17cb6ac8ac2f27fa0d07d983eb2170bf9ec6
Hcreak <hcreak@outlook.com>
2023-08-03 21:20:35 +0800

    change CI to ghcr

--- rocksdb-exporter/docker_rocksdb-exporter.sh
+++ rocksdb-exporter/docker_rocksdb-exporter.sh
@@ -1,5 +1,5 @@
 docker run -v /data:/rocksdb-exporter/data \
 	   -e LATEST_URL=https://prod-forge-us-west-2-chain-data-backup.s3.us-west-2.amazonaws.com/latest \
 	   -e REDIS_HOST=35.93.22.13 -e REDIS_PORT=9999 \
-	   --rm rocksdb-exporter
+	   --rm ghcr.io/findoranetwork/enterprise-web3:rocksdb-exporter-latest
 
